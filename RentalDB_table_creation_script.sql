868c4c9cdbbd4bec12440975d243823361f0fcb1
GH-Juuso <jjuuso.jaakkola@outlook.com>
2026-06-01 14:57:13 +0300

    Update RentalDB_table_creation_script.sql
    Typo

--- RentalDB_table_creation_script.sql
+++ RentalDB_table_creation_script.sql
@@ -87,7 +87,7 @@ ALTER TABLE Rental_transaction
 ADD CONSTRAINT FK_customer_id FOREIGN KEY(customer_id)
 REFERENCES Customer(customer_id)
 
-ALTER TABLE Rental_transactio
+ALTER TABLE Rental_transaction
 ADD CONSTRAINT FK_employee_id FOREIGN KEY(employee_id)
 REFERENCES Employee(employee_id)
 
