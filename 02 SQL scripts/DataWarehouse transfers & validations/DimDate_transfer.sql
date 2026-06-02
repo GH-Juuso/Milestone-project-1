651349ac97f0af910dc4e9fe3c433d59cd9279ee
Sb-Vtrs-elfamoso <56476423+Sb-Vtrs-elfamoso@users.noreply.github.com>
2026-06-02 11:48:37 +0200

    enddate ingestion fix

--- 02 SQL scripts/DataWarehouse transfers & validations/DimDate_transfer.sql
+++ 02 SQL scripts/DataWarehouse transfers & validations/DimDate_transfer.sql
@@ -25,5 +25,33 @@ WHERE startdate NOT IN (
     FROM RentalDataWarehouse.dbo.DimDate
 );
 
+-- end date insertion :
+INSERT INTO RentalDataWarehouse.dbo.DimDate
+(
+  date_key,
+  dayname,
+  week,
+  monthname,
+  year,
+  date
+)
+SELECT
+    CAST(CONVERT(char(8), enddate, 112) AS int) AS date_key,
+    DATENAME(WEEKDAY, enddate) AS dayname,
+    DATEPART(WEEK, enddate) AS week,
+    DATENAME(MONTH, enddate) AS monthname,
+    YEAR(enddate) AS year,
+    CAST(enddate AS date) AS date
+FROM (
+    SELECT DISTINCT CAST(enddate AS date) AS enddate
+    FROM RentalDatabase.dbo.Rental_transaction
+) d
+
+-- Blocking duplicates from being transferred
+WHERE enddate NOT IN (
+    SELECT date
+    FROM RentalDataWarehouse.dbo.DimDate
+);
+
 USE RentalDataWarehouse
 SELECT * FROM DimDate;
