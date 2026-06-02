2ffd8cd39babaf7abbca7a3df15e644dca67bded
GH-Juuso <jjuuso.jaakkola@outlook.com>
2026-06-02 11:44:13 +0300

    Add files via upload

---
+++ 02 SQL scripts/DataWarehouse transfers & validations/DimDate_transfer.sql
@@ -0,0 +1,26 @@
+INSERT INTO RentalDataWarehouse.dbo.DimDate
+(
+  date_key,
+  dayname,
+  week,
+  monthname,
+  year,
+  date
+)
+SELECT
+    CAST(CONVERT(char(8), startdate, 112) AS int) AS date_key,
+    DATENAME(WEEKDAY, startdate) AS dayname,
+    DATEPART(WEEK, startdate) AS week,
+    DATENAME(MONTH, startdate) AS monthname,
+    YEAR(startdate) AS year,
+    CAST(startdate AS date) AS date
+FROM (
+    SELECT DISTINCT CAST(startdate AS date) AS startdate
+    FROM RentalDatabase.dbo.Rental_transaction
+) d
+
+-- Blocking duplicates from being transferred
+WHERE startdate NOT IN (
+    SELECT date
+    FROM RentalDataWarehouse.dbo.DimDate
+);
