aae5d8bbc80e276f5be03b11df15044237e1d245
Sb-Vtrs-elfamoso <56476423+Sb-Vtrs-elfamoso@users.noreply.github.com>
2026-06-02 09:21:12 +0200

    Bug fix Equipment validation

--- 02 SQL scripts/DimEquipment_validation.sql
+++ 02 SQL scripts/DimEquipment_validation.sql
@@ -7,9 +7,9 @@ no quantity needed because we don't want that for market analysis
 USE RentalDataWarehouse;
 
 SELECT
-    e.EquipmentID AS SourceEquipmentID,
-    de.EquipmentID AS DWEquipmentID
-FROM DimEquipment de
-RIGHT OUTER JOIN RentalDatabase.dbo.Equipment e
-    ON de.EquipmentID = e.EquipmentID
-WHERE de.EquipmentID IS NULL;
+    e.equipment_id AS SourceEquipmentID,
+    de.equipment_id AS DWEquipmentID
+FROM DimEquipment as de
+RIGHT OUTER JOIN RentalDatabase.dbo.Equipment as e
+    ON de.equipment_id = e.equipment_id
+WHERE de.equipment_id IS NULL;
