e7e9d1d08fd08e9ee0dd41e7d1b1211e8c06a5e4
Sb-Vtrs-elfamoso <56476423+Sb-Vtrs-elfamoso@users.noreply.github.com>
2026-06-03 13:05:13 +0200

    date time DW update

--- 02 SQL scripts/DataWarehouse transfers & validations/DimDate_transfer.sql
+++ 02 SQL scripts/DataWarehouse transfers & validations/DimDate_transfer.sql
@@ -2,22 +2,26 @@ INSERT INTO RentalDataWarehouse.dbo.DimDate
 (
   date_key,
   dayname,
+  day_number,
   week,
   monthname,
+  month_number,
   year,
   date
 )
 SELECT
     CAST(CONVERT(char(8), startdate, 112) AS int) AS date_key,
     DATENAME(WEEKDAY, startdate) AS dayname,
+    DATEPART(DAY, startdate) AS day_number,
     DATEPART(WEEK, startdate) AS week,
     DATENAME(MONTH, startdate) AS monthname,
+    DATEPART(MONTH, startdate) AS month_number,
     YEAR(startdate) AS year,
     CAST(startdate AS date) AS date
 FROM (
     SELECT DISTINCT CAST(startdate AS date) AS startdate
     FROM RentalDatabase.dbo.Rental_transaction
-) d
+)d
 
 -- Blocking duplicates from being transferred
 WHERE startdate NOT IN (
@@ -30,16 +34,20 @@ INSERT INTO RentalDataWarehouse.dbo.DimDate
 (
   date_key,
   dayname,
+  day_number,
   week,
   monthname,
+  month_number,
   year,
   date
 )
 SELECT
     CAST(CONVERT(char(8), enddate, 112) AS int) AS date_key,
     DATENAME(WEEKDAY, enddate) AS dayname,
+    DATEPART(DAY, enddate) AS day_number,
     DATEPART(WEEK, enddate) AS week,
     DATENAME(MONTH, enddate) AS monthname,
+    DATEPART(MONTH, enddate) AS month_number,
     YEAR(enddate) AS year,
     CAST(enddate AS date) AS date
 FROM (
